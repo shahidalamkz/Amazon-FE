1c74d74b6e930faab2b4e0b620b37e7cd9875f66
shahidalamkz <77041444+shahidalamkz@users.noreply.github.com>
2024-02-13 13:56:36 +0530

    Update Main.tf

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.large"
   key_name               = "Mumbai"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
